f6a8a2ba61bb88f00b10b721be29c844a2fa892b
Leonardo Gomes <leonardo.nick.santos@gmail.com>
2020-08-16 12:35:13 -0300

    Classe 'Cor' alterada para tipo enumerado e finalizada. Classe 'Peca' iniciada.

--- XadrezConsole/XadrezConsole/Tabuleiro/Cor.cs
+++ XadrezConsole/XadrezConsole/Tabuleiro/Cor.cs
@@ -1,11 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace Tabuleiro
+﻿namespace Tabuleiro
 {
-    class Cor
+    enum Cor
     {
-
-    }
+        Branca,
+        Preta
+    };
 }
---
+++ XadrezConsole/XadrezConsole/Tabuleiro/Peca.cs
@@ -0,0 +1,12 @@
+﻿namespace Tabuleiro
+{
+    class Peca
+    {
+        public Posicao Posicao { get; set; }
+        public Cor Cor { get; protected set; }
+        public int QtdMovimentos { get; set; }
+        public Tabuleiro Tab { get; protected set; }
+
+
+    }
+}
