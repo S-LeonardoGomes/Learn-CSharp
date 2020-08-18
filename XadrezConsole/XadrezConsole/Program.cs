d681e22375295eaff26f5fbee9cf8ed45b6351d6
Leonardo Gomes <leonardo.nick.santos@gmail.com>
2020-08-18 13:08:56 -0300

    Melhorando a impressao do tabuleiro.

--- XadrezConsole/XadrezConsole/Program.cs
+++ XadrezConsole/XadrezConsole/Program.cs
@@ -8,9 +8,12 @@ namespace XadrezConsole
     {
         static void Main(string[] args)
         {
-            PosicaoXadrez pos = new PosicaoXadrez('c', 7);
-            Console.WriteLine(pos);
-            Console.WriteLine(pos.ToPosicao());
+            Tabuleiro tab = new Tabuleiro(8, 8);
+
+            tab.ColocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
+            tab.ColocarPeca(new Torre(tab, Cor.Branca), new Posicao(7, 0));
+
+            Tela.ImprimirTabuleiro(tab);
         }
     }
 }
