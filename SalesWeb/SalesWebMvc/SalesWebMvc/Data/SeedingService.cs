a0a1935df9ee6419e16dfff4a130fb6830185d9b
Leonardo Gomes <leonardo.nick.santos@gmail.com>
2021-01-03 19:36:24 -0300

    Tela simples de cadastro

--- SalesWeb/SalesWebMvc/SalesWebMvc/Data/SeedingService.cs
+++ SalesWeb/SalesWebMvc/SalesWebMvc/Data/SeedingService.cs
@@ -1,9 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using SalesWebMvc.Models;
+﻿using SalesWebMvc.Models;
 using SalesWebMvc.Models.Enums;
+using System;
+using System.Linq;
 
 namespace SalesWebMvc.Data
 {
@@ -18,9 +16,9 @@ namespace SalesWebMvc.Data
 
         public void Seed()
         {
-            if (_context.Department.Any()  || 
-                _context.Seller.Any()      ||
-                _context.SalesRecord.Any()  )
+            if (_context.Department.Any() ||
+                _context.Seller.Any() ||
+                _context.SalesRecord.Any())
             {
                 return; // DB has been seeded
             }
@@ -72,7 +70,7 @@ namespace SalesWebMvc.Data
 
             _context.Seller.AddRange(s1, s2, s3, s4, s5, s6);
 
-            _context.SalesRecord.AddRange(r1, r2, r3, r4, r5, r6, r7, r8, r9, r10, r11, r12, r13, 
+            _context.SalesRecord.AddRange(r1, r2, r3, r4, r5, r6, r7, r8, r9, r10, r11, r12, r13,
                 r14, r15, r16, r17, r18, r19, r20, r21, r22, r23, r24, r25, r26, r27, r28, r29, r30);
 
             _context.SaveChanges();
--- SalesWeb/SalesWebMvc/SalesWebMvc/Models/SalesRecord.cs
+++ SalesWeb/SalesWebMvc/SalesWebMvc/Models/SalesRecord.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using SalesWebMvc.Models.Enums;
+﻿using SalesWebMvc.Models.Enums;
+using System;
 
 namespace SalesWebMvc.Models
 {
