8af8fbac99af76bb8e4d169d3996674fc0e0eda8
Leonardo Gomes <leonardo.nick.santos@gmail.com>
2021-01-16 11:54:55 -0300

    Renomeando o projeto.

--- WebAPI/WebAPI_Macoratti/WebAPI_Macoratti/Controllers/WeatherForecastController.cs
+++ WebAPI/WebAPI_Macoratti/WebAPI_Macoratti/Controllers/WeatherForecastController.cs
@@ -5,7 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace WebAPI_Macoratti.Controllers
+namespace APICatalogo.Controllers
 {
     [ApiController]
     [Route("[controller]")]
--- WebAPI/WebAPI_Macoratti/WebAPI_Macoratti/WeatherForecast.cs
+++ WebAPI/WebAPI_Macoratti/WebAPI_Macoratti/WeatherForecast.cs
@@ -1,6 +1,6 @@
 using System;
 
-namespace WebAPI_Macoratti
+namespace APICatalogo
 {
     public class WeatherForecast
     {
