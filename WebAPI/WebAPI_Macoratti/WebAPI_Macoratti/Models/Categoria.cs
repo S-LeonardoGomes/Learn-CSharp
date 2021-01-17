24f32a2ae85fb9473ffd533e9bd46ff40b277285
Leonardo Gomes <leonardo.nick.santos@gmail.com>
2021-01-16 22:25:31 -0300

    Data annotation e migration.

--- WebAPI/WebAPI_Macoratti/WebAPI_Macoratti/Models/Categoria.cs
+++ WebAPI/WebAPI_Macoratti/WebAPI_Macoratti/Models/Categoria.cs
@@ -1,16 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
+//using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace APICatalogo.Models
 {
+    //[Table("Categorias")]
+    //Opcional, pois a entidade já está sendo mapeada
     public class Categoria
     {
+        [Key]
         public int CategoriaId { get; set; }
+
+        [Required]
+        [MaxLength(80)]
         public string Nome { get; set; }
+
+        [Required]
+        [MaxLength(300)]
         public string ImagemUrl { get; set; }
+
         public ICollection<Produto> Produtos { get; set; }
 
         public Categoria()
