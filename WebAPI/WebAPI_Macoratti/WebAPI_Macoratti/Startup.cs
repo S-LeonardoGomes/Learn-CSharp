de64ae8e8111ea8db6a2cf3548410cab8384967c
Leonardo Gomes <leonardo.nick.santos@gmail.com>
2021-01-31 20:57:18 -0300

    Criando logs personalizados em arquivo no formato txt

--- WebAPI/WebAPI_Macoratti/WebAPI_Macoratti/Startup.cs
+++ WebAPI/WebAPI_Macoratti/WebAPI_Macoratti/Startup.cs
@@ -1,6 +1,7 @@
 using APICatalogo.Context;
 using APICatalogo.Extensions;
 using APICatalogo.Filters;
+using APICatalogo.Logging;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -40,12 +41,17 @@ namespace APICatalogo
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILoggerFactory loggerFactory)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            loggerFactory.AddProvider(new CustomLoggerProvider(new CustomLoggerProviderConfiguration
+            {
+                LogLevel = LogLevel.Information
+            }));
 
             //adiciona o middleware de tratamento de erros
             app.ConfigureExceptionHandler();
