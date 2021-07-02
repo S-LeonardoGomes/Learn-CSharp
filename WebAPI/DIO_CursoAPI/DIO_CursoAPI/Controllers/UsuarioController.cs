6420f7f081bc9ea0b33a1471cb00d5bd6dbbe5f0
Leonardo Gomes <leonardo.nick.santos@gmail.com>
2021-07-02 17:55:00 -0300

    Iniciando controlador 'Usuario' com Actions 'Logar' e 'Registrar'. Criação de modelos de login e registro.

---
+++ WebAPI/DIO_CursoAPI/DIO_CursoAPI/Controllers/UsuarioController.cs
@@ -0,0 +1,27 @@
+﻿using DIO_CursoAPI.Models.Usuarios;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DIO_CursoAPI.Controllers
+{
+    [Route("api/v1/usuario")]
+    [ApiController]
+    public class UsuarioController : ControllerBase
+    {
+        [HttpPost("logar")]
+        public IActionResult Logar(LoginViewModelInput loginViewModelInput)
+        {
+            return Ok(loginViewModelInput);
+        }
+
+        [HttpPost("registrar")]
+        public IActionResult Registrar(RegistroViewModelInput registroViewModelInput)
+        {
+            return Created("", registroViewModelInput);
+        }
+    }
+}
