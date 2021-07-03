f07e7affba4388bd7921f39955a6a79a302781c5
Leonardo Gomes <leonardo.nick.santos@gmail.com>
2021-07-02 21:40:59 -0300

    Permitindo autorização via Swagger

--- WebAPI/DIO_CursoAPI/DIO_CursoAPI/Startup.cs
+++ WebAPI/DIO_CursoAPI/DIO_CursoAPI/Startup.cs
@@ -37,7 +37,31 @@ namespace DIO_CursoAPI
                 });
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "DIO_CursoAPI", Version = "v1" });
+                //c.SwaggerDoc("v1", new OpenApiInfo { Title = "DIO_CursoAPI", Version = "v1" });
+
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Description = "JWT Authorization header using the Bearer scheme (Example: 'Bearer 12345abcdef').",
+                    Name = "Authorization",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer"
+                });
+
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            }
+                        },
+                        Array.Empty<string>()
+                    }
+                });
             });
 
             var secret = Encoding.ASCII.GetBytes(Configuration.GetSection("JwtConfigurations:Secret").Value);
@@ -73,6 +97,8 @@ namespace DIO_CursoAPI
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseAuthentication();
 
             app.UseAuthorization();
 
