49137e00adf75f93dc4179b3bbe9664ca9bec235
Leonardo Gomes <leonardo.nick.santos@gmail.com>
2021-07-03 14:01:18 -0300

    Iniciando configuração de entidades de contexto

---
+++ WebAPI/DIO_CursoAPI/DIO_CursoAPI/Infraestruture/Data/CursoDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DIO_CursoAPI.Infraestruture.Data
+{
+    public class CursoDbContext : DbContext
+    {
+
+    }
+}
