9074797cc2084d2f023a4a469d9a27b249cb4710
Leonardo Gomes <leonardo.nick.santos@gmail.com>
2021-07-02 21:20:12 -0300

    Implementando CursoController e classes modelo

---
+++ WebAPI/DIO_CursoAPI/DIO_CursoAPI/Controllers/CursoController.cs
@@ -0,0 +1,45 @@
+﻿using DIO_CursoAPI.Models.Cursos;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace DIO_CursoAPI.Controllers
+{
+    [Route("api/v1/cursos")]
+    [ApiController]
+    [Authorize]
+    public class CursoController : ControllerBase
+    {
+        [SwaggerResponse(statusCode: 201, description: "Sucesso ao Cadastrar um curso")]
+        [SwaggerResponse(statusCode: 401, description: "Não autorizado")]
+        [HttpPost("")]
+        public IActionResult Post(CursoViewModelInput cursoViewModelInput)
+        {
+            var codigoUsuario = int.Parse(User.FindFirst(c => c.Type == ClaimTypes.NameIdentifier)?.Value);
+            return Created("", cursoViewModelInput);
+        }
+
+        [SwaggerResponse(statusCode: 200, description: "Sucesso ao obter os cursos")]
+        [SwaggerResponse(statusCode: 401, description: "Não autorizado")]
+        [HttpGet("")]
+        public IActionResult Get()
+        {
+            var cursos = new List<CursoViewModelOutput>();
+            //var codigoUsuario = int.Parse(User.FindFirst(c => c.Type == ClaimTypes.NameIdentifier)?.Value);
+            cursos.Add(new CursoViewModelOutput()
+            {
+                Login = "" /*codigoUsuario.ToString()*/,
+                Descricao = "teste",
+                Nome = "teste"
+            });
+
+            return Ok();
+        }
+    }
+}
