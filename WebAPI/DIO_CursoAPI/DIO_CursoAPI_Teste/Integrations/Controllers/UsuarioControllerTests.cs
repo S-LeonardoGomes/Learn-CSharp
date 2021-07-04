2c8a2b1bff2cc7701c381f1ebb0211e8fbf22b9f
Leonardo Gomes <leonardo.nick.santos@gmail.com>
2021-07-04 19:10:32 -0300

    Iniciando projeto de testes

---
+++ WebAPI/DIO_CursoAPI/DIO_CursoAPI_Teste/Integrations/Controllers/UsuarioControllerTests.cs
@@ -0,0 +1,20 @@
+﻿using DIO_CursoAPI;
+using Microsoft.AspNetCore.Mvc.Testing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DIO_CursoAPI_Teste.Integrations.Controllers
+{
+    public class UsuarioControllerTests
+    {
+        private readonly WebApplicationFactory<Startup> _factory;
+
+        public UsuarioControllerTests(WebApplicationFactory<Startup> factory)
+        {
+            _factory = factory;
+        }
+    }
+}
